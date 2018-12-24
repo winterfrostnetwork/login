d8490180d95a0d478614b877ba218114602dd875
winterfrostnetwork <46113494+winterfrostnetwork@users.noreply.github.com>
2018-12-24 22:11:51 +0000

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,117 @@
+﻿
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Chattr</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<link rel="stylesheet" href="/login/stylecolum.css"
+    <link href="/css/stylesphp.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to chattr. Hosted and owned by LawrenceTech. <img src="/profile.png" height="50" width="50" alt="" cl
+ass="icons"></h1>
+    </div>
+    <head>
+    <title>Chattr</title>
+    <style type="text/css">
+body             { padding-left:40px; background:#57767F; font-family:arial;}
+input, textarea  { font-family: courier new; font-size: 12px; }
+#content         { width:800px; text-align:center; margin-left:60px; }
+
+#chatwindow      { border:1px solid #aaaaaa; padding:4px; background:#232D2F; color:white;}
+#chatnick        { border-bottom:1px solid #aaaaaa; padding:4px; background:#FFFFFF;}
+#chatmsg         { border-bottom:1px solid #aaaaaa; padding:4px; background:#FFFFFF; }
+
+#info            { text-align:center; padding-left:0px; font-family:verdana; }
+#info td         { font-size:12px; padding-right:10px; color:#DFDFDF;  }
+#info .small     { font-size:10px; padding-left:10px; padding-right:0px; }
+
+#info a          { text-decoration:none; color:white; }
+#info a:hover    { text-decoration:underline; color:#CF9700; }
+#button 	 { font-family:verdana; }
+</style>
+    </head>
+    <body>
+	<h1>Now at 50 USERSSSSS!!!</h1>
+	<style>
+	::placeholder { 
+		color: black; opacity: 1; /* Firefox */ 
+	}
+	</style>
+        <br>
+        <div id="content">
+            <textarea id="chatwindow" rows="19" cols="162" readonly></textarea><br>
+
+            <input id="chatnick" type="text" size="9" maxlength="10" placeholder="username">&nbsp;
+            <input id="chatmsg" type="text" size="80" maxlength="80"  onkeyup="keyup(event.keyCode);" placeholder="message">
+            <input type="button" class="btn btn-success" value="send" onclick="submit_msg();" style="cursor:pointer;border:1px solid gray;"><br><br>
+        </div>
+</body>
+</html>
+
+<script type="text/javascript">
+var nick_maxlength=8;
+var http_request=false;
+var http_request2=false;
+var intUpdate;
+
+/* http_request for writing */
+function ajax_request(url){http_request=false;if(window.XMLHttpRequest){http_request=new XMLHttpRequest();if(http_request.overrideMimeType){http_request.overrideMimeType('text/xml');}}else if(window.ActiveXObject){try{http_request=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{http_request=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}
+if(!http_request){alert('Giving up :( Cannot create an XMLHTTP instance');return false;}
+http_request.onreadystatechange=alertContents;http_request.open('GET',url,true);http_request.send(null);}
+function alertContents(){if(http_request.readyState==4){if(http_request.status==200){rec_response(http_request.responseText);}else{}}}
+
+/* http_request for reading */
+function ajax_request2(url){http_request2=false;if(window.XMLHttpRequest){http_request2=new XMLHttpRequest();if(http_request2.overrideMimeType){http_request2.overrideMimeType('text/xml');}}else if(window.ActiveXObject){try{http_request2=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{http_request2=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}
+if(!http_request2){alert('Giving up :( Cannot create an XMLHTTP instance');return false;}
+http_request2.onreadystatechange=alertContents2;http_request2.open('GET',url,true);http_request2.send(null);}
+function alertContents2(){if(http_request2.readyState==4){if(http_request2.status==200){rec_chatcontent(http_request2.responseText);}else{}}}
+
+/* chat stuff */
+chatmsg.focus()
+var show_newmsg_on_bottom=1;     /* set to 0 to let new msg´s appear on top */
+var waittime=3000;        /* time between chat refreshes (ms) */
+
+intUpdate=window.setTimeout("read_cont();", waittime);
+chatwindow.value = "loading...";
+
+function read_cont()         { zeit = new Date(); ms = (zeit.getHours() * 24 * 60 * 1000) + (zeit.getMinutes() * 60 * 1000) + (zeit.getSeconds() * 1000) + zeit.getMilliseconds(); ajax_request2("chat.txt?x=" + ms); }
+function display_msg(msg1)     { chatwindow.value = msg1.trim(); }
+function keyup(arg1)         { if (arg1 == 13) submit_msg(); }
+function submit_msg()         { clearTimeout(intUpdate); if (chatnick.value == "") { check = prompt("please enter username:"); if (check === null) return 0; if (check == "") check="..."; chatnick.value=check; } if (chatnick.value.length > nick_maxlength) chatnick.value=chatnick.value.substring(0,nick_maxlength); spaces=""; for(i=0;i<(nick_maxlength-chatnick.value.length);i++) spaces+=" "; v=chatwindow.value.substring(chatwindow.value.indexOf("\n")) + "\n" + chatnick.value + spaces + "| " + chatmsg.value; if (chatmsg.value != "") chatwindow.value=v.substring(1); write_msg(chatmsg.value,chatnick.value); chatmsg.value=""; intUpdate=window.setTimeout("read_cont();", waittime);}
+function write_msg(msg1,nick1)     { ajax_request("w.php?m=" + escape(msg1) + "&n=" + escape(nick1)); }
+function rec_response(str1)     { }
+
+function rec_chatcontent(cont1) {
+    if (cont1 != "") {
+        out1 = unescape(cont1);
+        if (show_newmsg_on_bottom == 0) { out1 = ""; while (cont1.indexOf("\n") > -1) { out1 = cont1.substr(0, cont1.indexOf("\n")) + "\n" + out1; cont1 = cont1.substr(cont1.indexOf("\n") + 1); out1 = unescape(out1); } }
+        if (chatwindow.value != out1) { display_msg(out1); }
+        intUpdate=window.setTimeout("read_cont()", waittime);
+    }
+}
+</script>
+    <center><script type="text/javascript" src="//counter.websiteout.net/js/2/0/541/0"></script></center>
+    <p><a href="admin/adminlogin.php" class="btn btn-success">Admin</a></p>
+    <p><a href="gs/gs.php" class="btn btn-success">Games</a></p>
+    <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+</body>
+</html>
